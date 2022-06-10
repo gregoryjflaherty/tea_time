066ec84e454cf6182d62d661c4041ca4f2d7a653
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-06-10 10:45:52 -0400

    Feat: add authorization helper methods in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+  
+  def current_customer
+    if session[:customer_id]
+      @current_customer = Customer.find(session[:customer_id])
+    end
+  end
+
+  def authorize
+    if !current_customer
+      render json: {message: "Unauthorized: Please log in first"}, status: 401
+    end
+  end
 end
