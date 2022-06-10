cc25280da182cc58ee18cf133721bd6f95768968
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-06-10 12:27:30 -0400

    Refactor: add timestamps to subscription serializer

--- app/serializers/subscriptions_serializer.rb
+++ app/serializers/subscriptions_serializer.rb
@@ -1,4 +1,4 @@
 class SubscriptionsSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :customer_id, :tea_id, :price, :status, :frequency
+  attributes :customer_id, :tea_id, :price, :status, :frequency, :created_at, :updated_at
 end
